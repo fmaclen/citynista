765850eb0d18f44bae8bb4c173aba82dfca2dcad
Fernando Maclen <fmaclen@gmail.com>
2025-10-15 16:12:10 -0400

    feat: move OSM import button to toolbar

--- src/lib/components/EditorToolbar.svelte
+++ src/lib/components/EditorToolbar.svelte
@@ -2,12 +2,16 @@
 	import DrawButton from './toolbar/DrawButton.svelte';
 	import SelectButton from './toolbar/SelectButton.svelte';
 	import ClearButton from './toolbar/ClearButton.svelte';
+	import OSMButton from './toolbar/OSMButton.svelte';
+	import { Separator } from '$lib/components/ui/separator';
 </script>
 
 <div class="fixed bottom-8 left-1/2 z-50 -translate-x-1/2">
-	<nav class="flex flex-row gap-2">
+	<nav class="flex flex-row items-center gap-2">
 		<DrawButton />
 		<SelectButton />
 		<ClearButton />
+		<Separator orientation="vertical" class="h-8" />
+		<OSMButton />
 	</nav>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import EditorToolbar from '$lib/components/EditorToolbar.svelte';
-	import OSMLoader from '$lib/components/OSMLoader.svelte';
 	import { setGraphContext } from '$lib/context/graph.svelte';
 	import { Graph } from '$lib/graph/graph.svelte';
-	import { setupCanvas, setMode } from '$lib/canvas';
+	import { setupCanvas } from '$lib/canvas';
 	import { restoreGraph } from '$lib/graph/restore';
-	import { fetchOSMData } from '$lib/osm/import';
-	import { importOSMToGraph } from '$lib/osm/import-to-graph';
 
 	let canvasElement: HTMLCanvasElement;
 	const ctx = setGraphContext();
@@ -21,14 +18,6 @@
 			restoreGraph(ctx.graph, canvas, savedData);
 		}
 	});
-
-	async function handleLoadOSM() {
-		const osmData = await fetchOSMData(25.618, -80.3465, 25.62, -80.344);
-		console.log('Loaded OSM data:', osmData);
-		if (ctx.canvas) {
-			importOSMToGraph(osmData, ctx.graph, ctx.canvas);
-		}
-	}
 </script>
 
 <svelte:head>
@@ -37,7 +26,6 @@
 
 <div class="h-screen w-screen overflow-hidden bg-[#2a2a2a]">
 	<EditorToolbar />
-	<OSMLoader onLoad={handleLoadOSM} />
 	<canvas id="canvas" bind:this={canvasElement}></canvas>
 </div>
 
